f1289bd21bc289d3a74f20bcd1ed7e9e53a552d6
Dima <playcssv34@gmail.com>
2021-01-30 21:21:31 +0300

    my second practic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-while (num < 55) {
-    console.log(num);
-    num++;
+if (numberOfFilms < 10) {
+    console.log('Просмотрено мало фильмов');
+} else if (10 < numberOfFilms < 30) {
+    console.log('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let i = 0;
+do {
+    i++;
+    let askFilmPerson1 = prompt("Один из последних просмотренных фильмов?", ""),
+        askFilmPerson2 = +prompt("На сколько оцените его?", "");
+    if (askFilmPerson1 != null && askFilmPerson2 != null && askFilmPerson1 != '' && askFilmPerson2 != '' && askFilmPerson1 < 50) {
+        personalMovieDB.movies[askFilmPerson1] =  askFilmPerson2;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+} while (i < 2);
+
+   
+    
+
+
+console.log(personalMovieDB);
