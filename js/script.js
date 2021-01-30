bd1ec3ba7ff01ed2329e6f2b1ccb34844355b308
Dima <playcssv34@gmail.com>
2021-01-30 17:41:51 +0300

    add js file

--- js/script.js
+++ js/script.js
@@ -1,21 +1,12 @@
 "use strict";
 
-// alert('helloooo');
+console.log(4 + +'5');
 
-// const result = confirm("Are you here?");
-// console.log(result);
+let incr = 10,
+    decr = 20;
 
-// const answer = +prompt("ВАм есть 18?", "");
-// console.log(typeof(answer));
+// incr--;
+// decr++;
 
-// const answers = [];
-
-// answers[0] = prompt("Как ваше имя?", "");
-// answers[1] = prompt("Сколько вам лет?", "");
-// answers[2] = prompt("Где вы живете", "");
-
-// document.write(answers);
-
-const name = 'Ivan';
-
-alert(`Hello,${name}`)
+console.log(incr);
+console.log(decr);
