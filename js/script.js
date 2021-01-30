2b121f1182e0c54932bbcf4eeca56fc2695e55d2
Dima <playcssv34@gmail.com>
2021-01-30 19:56:52 +0300

    final my first programm

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 "use strict";
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+console.log(personalMovieDB.count);
 
 let askFilmPerson1 = prompt("Один из последних просмотренных фильмов?", ""),
-    askFilmPerson2 = prompt("На сколько оцените его?", "");
+    askFilmPerson2 = +prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies = {askFilmPerson1: askFilmPerson2};
+personalMovieDB.movies[askFilmPerson1] =  askFilmPerson2;
+console.log(personalMovieDB.movies);
 
 
