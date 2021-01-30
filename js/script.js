ddfb36fca187645653b16ed4d3eabb45785e0643
Dima <playcssv34@gmail.com>
2021-01-30 19:38:55 +0300

    add object personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 "use strict";
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: "",
+    actors: "",
+    genres: [],
+    privat: false
+};
