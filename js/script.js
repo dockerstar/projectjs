52c82b3385d1a149342c92f361b4817e74e59678
Dima <playcssv34@gmail.com>
2021-01-30 19:47:12 +0300

    add askFilmPerson

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@ let numberOfFilms = prompt('Сколько фильмов вы уже посмо
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: "",
-    actors: "",
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
+
+let askFilmPerson1 = prompt("Один из последних просмотренных фильмов?", ""),
+    askFilmPerson2 = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies = {askFilmPerson1: askFilmPerson2};
+
+
