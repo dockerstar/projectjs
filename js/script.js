bf7ba4fba789c90dba9db8fa78924e0afbbdfb48
Dima <playcssv34@gmail.com>
2021-01-30 19:33:28 +0300

    add numberOfFilms

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 "use strict";
 
-console.log(4 + +'5');
-
-let incr = 10,
-    decr = 20;
-
-// incr--;
-// decr++;
-
-console.log(incr);
-console.log(decr);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
