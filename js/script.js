373c9c104f4e335a4169be706e8eccffae645a9b
Dima <playcssv34@gmail.com>
2021-01-30 17:24:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert('helloooo');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("ВАм есть 18?", "");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Сколько вам лет?", "");
+// answers[2] = prompt("Где вы живете", "");
+
+// document.write(answers);
+
+const name = 'Ivan';
+
+alert(`Hello,${name}`)
