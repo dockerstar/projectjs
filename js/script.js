d06fcd433e9ba14ab88a25e72a792aa2a86c3f40
Dima <playcssv34@gmail.com>
2021-02-01 22:54:44 +0300

    method massiv

--- js/script.js
+++ js/script.js
@@ -1,40 +1,25 @@
 "use strict"
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log('test');
-    }
-};
+const arr = [1, 2, 3, 4, 5];
 
-// const {border, bg} = options.colors;
-// console.log(bg);
-
-// options.makeTest();
-
-// console.log(Object.keys(options).length);
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} in ${arr}`);
+// });
 
 
-// console.log(options.name);
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
 
-// delete options.name;
-// let counter = 0;
-
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//             // counter++;
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         // counter++;
-//     }
+// for ( let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
 
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+
+const str = prompt('', '');
+const products = str.split(', ');
+console.log(products);
