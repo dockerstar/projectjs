10d3684795b1e00defe6b3bc73fc3a1ed0a49c3d
Dima <playcssv34@gmail.com>
2021-02-01 22:04:43 +0300

    Object and distruction

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
 "use strict"
 
-function first() {
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('test');
+    }
+};
 
-function second() {
-    console.log(2);
-}
+// const {border, bg} = options.colors;
+// console.log(bg);
 
-first();
-second();
+// options.makeTest();
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// console.log(Object.keys(options).length);
 
-learnJS('JS', function(){
-    console.log('Я прошел этот урок');
-});
+
+// console.log(options.name);
+
+// delete options.name;
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             // counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         // counter++;
+//     }
+// }
+
