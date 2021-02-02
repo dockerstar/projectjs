7074133914dfdcfd4115befa3abb89041d74926c
Dima <playcssv34@gmail.com>
2021-02-02 20:47:02 +0300

    spread

--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
 "use strict"
 
-const arr = [1, 2, 3, 4, 5];
+const add = {
+    d: 17,
+    e: 20
+};
 
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} in ${arr}`);
-// });
+const numbers = {
+    a: 2,
+    b: 10,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newObj = Object.assign({}, add);
+
+newObj.d = 10;
+
+// console.log(add);
+// console.log(newObj);
+
+const oldArray = ['a', 'b', 'c'];
+const array = [...oldArray];
+console.log(array);
+const newArray = oldArray.slice();
+
+// console.log(newArray);
+
+const video = ['yt', 'vm', 'rt'],
+      blogs = ['wp', 'lj', 'bg'],
+      internet = [...video, ...blogs, 'vk'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
 
 
-// arr.pop();
-// arr.push(10);
-// console.log(arr);
+const obj = {
+    one: 1,
+    two: 2
+};
 
-// for ( let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+const newObjs = {...obj};
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+console.log(newObjs);
 
 
-const str = prompt('', '');
-const products = str.split(', ');
-console.log(products);
